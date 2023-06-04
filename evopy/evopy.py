Big fixes: * force strength now passes to offspring. * mutating force_strength no longer happens in a local function. * disabled inertia. (normalization makes forces too large)

evopy/evopy.py
@@ -105,7 +105,7 @@ class EvoPy:
             if self.reporter is not None:
                 mean = np.mean([x.fitness for x in population])
                 std = np.std([x.fitness for x in population])
-                self.reporter(ProgressReport(generation, self.evaluations, best.genotype, best.fitness, mean, std, time.time() - start_time, should_stop))
+                self.reporter(ProgressReport(generation, self.evaluations, best, best.genotype, best.fitness, mean, std, time.time() - start_time, should_stop))
 
             if should_stop:
                 break

evopy/individual.py
@@ -1,5 +1,6 @@
 """Module containing the individuals of the evolutionary strategy algorithm."""
 import numpy as np
+from matplotlib import pyplot as plt
 
 from evopy.strategy import Strategy
 from evopy.utils import random_with_seed
@@ -84,7 +85,14 @@ class Individual:
 
         mean = self.genotype
         if self.force_strength > 0:
+
+            # scale_factor = self.random.randn() * np.sqrt(1 / (2 * self.length))
+            # self.force_strength = max(self.force_strength * np.exp(scale_factor), self._EPSILON)
+
+            return mean + calculate_forces(self.genotype, self.force_strength).flatten()
+
             change_vector = calculate_forces(self.genotype, self.force_strength).flatten() + self.inertia
+
             # normalize it
             change = change_vector/np.linalg.norm(change_vector)
             result = mean + change
@@ -113,7 +121,7 @@ class Individual:
         scale_factor = self.random.randn() * np.sqrt(1 / (2 * self.length))
         new_parameters = [max(self.strategy_parameters[0] * np.exp(scale_factor), self._EPSILON)]
         new_genotype = self._handle_oob_indices(new_genotype)
-        return Individual(new_genotype, self.strategy, new_parameters, bounds=self.bounds, random_seed=self.random)
+        return Individual(new_genotype, self.strategy, new_parameters, force_strength=self.force_strength, bounds=self.bounds, random_seed=self.random)
 
     def _reproduce_multiple_variance(self):
         """Create a single offspring individual from the set genotype and strategy.
@@ -165,3 +173,9 @@ class Individual:
         new_genotype = self.genotype + T @ self.random.randn(self.length)
         new_genotype = self._handle_oob_indices(new_genotype)
         return Individual(new_genotype, self.strategy, new_variances + new_rotations, bounds=self.bounds)
+
+    def plot_distribution_mean(self):
+        points = np.reshape(self.genotype, (-1, 2))
+        delta = np.reshape(self._distribution_mean()-self.genotype, (-1, 2))
+        for p0, p1, f0, f1 in zip(points[:, 0], points[:, 1], delta[:, 0], delta[:, 1]):
+            plt.arrow(p0, p1, f0, f1, head_width=0.02, head_length=0.02, fc='k', ec='k')

forces.py
@@ -18,25 +18,24 @@ SAFE_DIVISION_EPSILON = 1e-8
     weights = normalize(1/distances)
     return np.einsum('ijk,ij->ik', differences, weights).flatten()'''
 
-#Mutate the strength parameter below to change the strength of the forces
-def mutate_force_strength(strength, mutate_rate=0.05):
-    #if np.random.rand() <= mutate_rate:
-    return strength + np.random.randn() * 0.01
-    #return strength
-
-def calculate_forces(genotype, strength=0.25):
+def calculate_forces(genotype, strength=0.25, k=1):
     """
-    Calculate forces by only considering the closest neighbours
+    Calculate forces by only considering the k closest neighbours
     """
+    HIGH_VALUE = 10
     if strength <= 0:
         return np.zeros(genotype.shape)
     points = np.reshape(genotype, (-1, 2))
     distances = euclidean_distances(points, squared=True)
-    np.fill_diagonal(distances, 10)
-    closest_neighbours = np.argmin(distances, axis=-1)
-    weights = 1/(distances[tuple(zip(*enumerate(closest_neighbours)))] + SAFE_DIVISION_EPSILON)
-    strength = mutate_force_strength(strength)
-    return strength*weights[:, np.newaxis] * (points - points[closest_neighbours])
+    np.fill_diagonal(distances, HIGH_VALUE)
+    forces = np.zeros(points.shape)
+    for _ in range(k):
+        closest_neighbours = np.argmin(distances, axis=-1)
+        indices = tuple(zip(*enumerate(closest_neighbours)))
+        weights = 1/(distances[indices] + SAFE_DIVISION_EPSILON)
+        distances[indices] = HIGH_VALUE
+        forces += strength/k*weights[:, np.newaxis] * (points - points[closest_neighbours])
+    return forces
 
 def plot_forces(genotype, strength=0.25):
     points = np.reshape(genotype, (-1, 2))